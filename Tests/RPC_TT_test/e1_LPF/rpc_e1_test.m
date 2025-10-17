f3d636355e0daaa1f79f9b07b326bfbc6cdb425c
williamin18 <ziyuan.wang@mail.mcgill.ca>
2025-10-17 18:02:30 -0400

    Update rpc_e1_test.m

--- Tests/RPC_TT_test/e1_LPF/rpc_e1_test.m
+++ Tests/RPC_TT_test/e1_LPF/rpc_e1_test.m
@@ -5,6 +5,13 @@ load('Tests/RPC_TT_test/e1_LPF/rpc_e1_samples.mat')
 m = 3;
 x_train = training_samples2(1:3000,:);
 y_train = vouts_train2(1:3000,:);
-y_predict = rpc_total(x_train,y_train(:,1:50),test_samples,3,'Hermite');
+y_predict = rpc_total(x_train,y_train(:,100),test_samples,3,'Hermite');
 
-norm(y_predict-vouts_test(:,1:50))/norm(vouts_test(:,1:50))
+norm(y_predict-vouts_test(:,100))/norm(vouts_test(:,100))
+
+f = figure(5);
+Hmc = histogram(abs(vouts_test(:,100)) ,50,'Normalization','pdf', 'DisplayStyle','bar', 'FaceColor',[0.7 0.7 0.7]);
+hold on
+grid on
+[N_mc,edges] = histcounts(abs(y_predict), 'Normalization', 'pdf');
+plot(edges(1:end-1)+(edges(2)-edges(1))/2,N_mc,'b:','LineWidth',2)
