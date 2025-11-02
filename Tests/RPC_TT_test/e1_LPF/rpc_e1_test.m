ebe03cdb099f7c60707de3aedb2bf74c415a2f32
williamin18 <ziyuan.wang@mail.mcgill.ca>
2025-11-02 18:44:27 -0500

    ranks are high

--- Tests/RPC_TT_test/e1_LPF/rpc_e1_test.m
+++ Tests/RPC_TT_test/e1_LPF/rpc_e1_test.m
@@ -11,15 +11,14 @@ y_train = vouts_train2(1:1000,:);
 
 [~,d] = size(xi_train);
 N = [(m+1)*ones(d,1); 2];
-x = TTrand(N,2);
+x = TTrand(N,3);
 x = TTorthogonalizeLR(x);
-for i = 2:d
-    x{i}(3:end,:) = 0;
-end
-x{d} = x{d}/norm( x{d},'fro');
+x{d+1} = x{d+1}/norm( x{d+1},'fro');
+x = TTorthogonalizeRL(x);
+
 
 [y_predict2,RPC_coefficients,n_iterations] = rpc_TT(xi_train,y_train(:,100),x,test_samples,m,'Hermite',...
-    0.4,0.1,1e-3,0.9,3);
+    0.8,0,1e-3,0.9,5);
 
 norm(y_predict2-vouts_test(:,100))/norm(vouts_test(:,100))
 
