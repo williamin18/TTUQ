0bc9f8bfcef344da4d7384cf757a5f9fb2ed2d55
williamin18 <ziyuan.wang@mail.mcgill.ca>
2025-11-30 15:45:53 -0500

    Update rpc_e1_test.m

--- Tests/RPC_TT_test/e1_LPF/rpc_e1_test.m
+++ Tests/RPC_TT_test/e1_LPF/rpc_e1_test.m
@@ -6,19 +6,19 @@ m = 1;
 xi_train = training_samples1(1:1000,:);
 y_train = vouts_train1(1:1000,:);
 
-[y_predict2,n_rpc,d_rpc] = rpc_total(xi_train,y_train(:,100),test_samples,3,'Hermite',1,0,5e-3);
-norm(y_predict2-vouts_test(:,100))/norm(vouts_test(:,100))
+% [y_predict2,n_rpc,d_rpc] = rpc_total(xi_train,y_train(:,100),test_samples,3,'Hermite',1,0,5e-3);
+% norm(y_predict2-vouts_test(:,100))/norm(vouts_test(:,100))
 
-% [~,d] = size(xi_train);
-% N = [(m+1)*ones(d,1); 2];
-% x = TTrand(N,3);
-% x = TTorthogonalizeLR(x);
-% x{d+1} = x{d+1}/norm( x{d+1},'fro');
-% x = TTorthogonalizeRL(x);
-% 
-% 
-% [y_predict2,RPC_coefficients,n_iterations] = rpc_TT(xi_train,y_train(:,100),x,test_samples,m,'Hermite',...
-%     0.3,0.01,1e-3,0.9,8);
+[~,d] = size(xi_train);
+N = [(m+1)*ones(d,1); 2];
+x = TTrand(N,3);
+x = TTorthogonalizeLR(x);
+x{d+1} = x{d+1}/norm( x{d+1},'fro');
+x = TTorthogonalizeRL(x);
+
+
+[y_predict2,RPC_coefficients,n_iterations] = rpc_TT(xi_train,y_train(:,100),x,test_samples,m,'Hermite',...
+    0.3,0.01,1e-3,0.9,8);
 
 norm(y_predict2-vouts_test(:,100))/norm(vouts_test(:,100))
 
