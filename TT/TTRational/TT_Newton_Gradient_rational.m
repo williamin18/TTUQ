2ac81769d3447586c9241b130c3016760d0cc474
Ziyuan Wang <ziyuan.wang@mail.mcgill.ca>
2026-01-17 21:18:04 +0800

    rational 3.0

---
+++ TT/TTRational/TT_Newton_Gradient_rational.m
@@ -0,0 +1,79 @@
+function [V,dUx,dy] = TT_Newton_Gradient_rational(A,Cb,U,y,residual,beta,dx_old,lambda)
+% To solve (Ax)./(1+Cy) = b, we find Ax - (C.*b)y = b. Each iteration we
+% update x and y by solving dx - (C.*b)dy = r
+
+[d,m,r] = TTsizes(U);
+
+
+%Right Orthogonalize, find every non-orthogal Ux to compute the seach
+%directions dUx
+[Ux,V] = TTmuOrthogonalizeRL(U);
+
+
+
+[~,yl] = Ax_left(A,U,d);
+[~,yr] = Ax_right(A,V,1);
+[n_samples,~] = size(A{1});
+
+dUx = cell(d,1);
+
+%solve the seach directions
+for i = 1:d
+    for j = 1:m(i)
+        temp = residual.*A{i}(:,j);
+        temp = yr{i}.*repelem(temp,1,r(i+1));
+        dUx{i}((j-1)*r(i)+1:j*r(i),:) = (temp'*yl{i})';        
+        % dUx{i}((j-1)*r(i)+1:j*r(i),:) = (yr{i}'* diag(residual.*reshape(A(i,j,:),n_samples,1))*yl{i})';
+    end
+end
+
+%solve A*dx for computating step sizes
+Adx = TTmuOrthogonalAx(A,dUx,yl,yr,m,d,r);
+
+if beta <= 0
+    %no momentum
+    Adx = [Adx Cb];
+    %find Gram matrix with regularization
+    reg_matrix = TTRegMatrix(dUx,U,V,lambda);
+    reg_matrix = [reg_matrix zeros(d,d);zeros(d,d) lambda^2*eye(d)];
+
+    reg_vec = zeros(2*d,1);
+    for i = 1:d
+        reg_vec(i) = -lambda^2*sum(conj(dUx{i}).* Ux{i},"all");
+        reg_vec(i+d) = -lambda^2*y;
+    end
+    
+    %solve step sizes, update seach directions
+    alpha = (Adx'*Adx+reg_matrix)\(Adx'*residual+reg_vec);
+    for i = 1:d
+        dUx{i} = alpha(i)*dUx{i};
+    end
+    dy = alpha(d+1:2*d);
+else
+    %with momentum, project the search directions from the previous iteration
+    dU_old = TT_Riemannian_projection(U,V,dx_old);
+    Adx_old = TTmuOrthogonalAx(A,dU_old,yl,yr,m,d,r);
+    Adx = [Adx Adx_old Cb];
+
+    %find Gram matrix with regularization
+    reg_matrix = TTRegMatrix(dUx,U,V,lambda,dU_old);
+    reg_matrix = [reg_matrix zeros(2*d,d);zeros(d,2*d) lambda^2*eye(d)];
+    reg_vec = zeros(3*d,1);
+
+    for i = 1:d
+        reg_vec(i) = -lambda^2*sum(conj(dUx{i}).* Ux{i},"all");
+        reg_vec(i+d) = -lambda^2*sum(conj(dU_old{i}).* Ux{i},"all");
+        reg_vec(i+2*d) = -lambda^2*y;
+    end
+    %solve step sizes, update seach directions
+    alpha = (Adx'*Adx+reg_matrix)\(Adx'*residual+reg_vec);
+    for i = 1:d
+        dUx{i} = alpha(i)*dUx{i}+alpha(i+d)*dU_old{i};
+    end
+    dUx{d} = dUx{d} + alpha(2*d+1)*Ux{d};
+    dy = alpha(2*d+1:3*d);
+
+end
+
+end
+
---
+++ TT/TTRational/TT_Newton_rational6.m
@@ -0,0 +1,75 @@
+function [x,training_err,test_err,epoch] = TT_Newton_rational4(A,x,b,rank,tol,max_iterations,A_test,b_test,lambda)
+%find x_n and x_d that minimizes || (A*x_1)./(1+A*x_2)-b||, x = [x1; x2]
+
+
+%parameter initialization
+break_counter = 0;
+break_limit = 5;
+err_old = 100;
+
+
+x = TTorthogonalizeLR(x);
+d = length(A);
+[n_samples,~] = size(A{1});
+[~,m,~] = TTsizes(x);
+
+A_linear = [A;{[ones(n_samples,1) -b]}];
+for i = 1:d
+    A_linear{i}(n_samples+1,:) = [1 zeros(1,m(i)-1)];
+end
+A_linear{d+1}(n_samples+1,:) = [0 100];
+b_linear = [zeros(n_samples,1);100];
+
+
+beta = 0;
+dx_TT = 0;
+
+for epoch = 1:max_iterations
+
+    
+
+    [~,~,tt_ranks] = TTsizes(x);
+    % x_d = x(1:d);
+    % x_d{d} = x_d{d}*x{d+1}(tt_ranks(d+1)+1:2*tt_ranks(d+1));
+    % rho = 1./(multi_r1_times_TT(A,x_d));
+    % rho = rho/max(abs(rho));
+    % A_linear{d+1}(1:n_samples,:) = [rho -rho.*b];
+    r = b_linear - multi_r1_times_TT(A_linear,x);
+    test_r1 =  norm(r);
+    
+    %Compute Newton updates for each core
+    [V,dUx] = TT_Newton_Gradient(A_linear,x,r,beta,dx_TT,lambda);
+    %Update x by TT-structure update
+    dx_TT = TT_Riemannian_fromGTensor(x,V,dUx);
+    beta = 1;
+    x = TT_Riemannian_update(x,V,dUx,1,rank);
+    [~,~,tt_ranks] = TTsizes(x);
+
+    % [test_r1 norm(b_linear - multi_r1_times_TT(A_linear,x))]
+
+    x_n = x(1:d);
+    x_n{d} = x_n{d}*x{d+1}(1:tt_ranks(d+1));
+    x_d = x(1:d);
+    x_d{d} = x_d{d}*x{d+1}(tt_ranks(d+1)+1:2*tt_ranks(d+1));
+    r_train = b - multi_r1_times_TT(A,x_n)./multi_r1_times_TT(A,x_d);
+    r_test = b_test - multi_r1_times_TT(A_test,x_n)./multi_r1_times_TT(A_test,x_d);
+    training_err = norm(r_train)/norm(b);
+    test_err = norm(r_test)/norm(b_test);
+
+    [test_r1 training_err test_err];
+    if test_err < tol 
+        break
+    end
+
+    if   err_old-training_err < tol/1000*d
+        break_counter = break_counter+1;
+        if break_counter > break_limit
+            break
+        end
+    else
+        break_counter = 0;
+    end
+    err_old = training_err;
+end
+end
+
