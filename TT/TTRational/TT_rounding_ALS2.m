a7dfc07d9ee7ab40c0add4bda458ef9f5e3cba4c
williamin18 <ziyuan.wang@mail.mcgill.ca>
2026-03-06 17:25:51 -0500

    Eckart-Young-Mirsky (EYM) Theorem rounding

--- TT/TTRational/TT_rounding_ALS2.m
+++ TT/TTRational/TT_rounding_ALS2.m
@@ -15,29 +15,29 @@ yl{1} = ones(n_samples,1);
 for i = 1:d-1
     xi = TTcore_LS(yl{i},yr{i},A{i},b,lambda);
     %use SVD rounding
-    [U,~,~] = svd(xi,"econ");
-    r(i+1) = min(r(i+1),r_max);
-    x{i} = U(:,1:r(i+1));
+
+    
     %Use Erhard rounding
     xi = reshape(x{i},[r(i), m(i), r(i+1)]);
     xi = reshape(permute(xi, [2 1 3]),m(i),[]);
-    Axi = A{i}*xi;
-    [U,~,~] = svd(Axi,'econ');
+    R = qr(A{i});
+    Rxi = R*xi;
+    Rxi = reshape(Rxi,[m(i),r(i),r(i+1)]);
+    Rxi = reshape(permute(Rxi, [2 1 3]),r(i)*m(i),[]);
+    [U,S,V] = svd(Rxi,'econ');
     r(i+1) = min(r(i+1),r_max);
-    xi2 = A{i}\U(:,1:r(i+1));
-    xi2 = reshape(xi2,[])
-
-    %-----------------------------------
-    xi = reshape(x{i},[r(i), m(i), r(i+1)]);
-    xi = reshape(permute(xi, [2 1 3]),m(i),[]);
+    Rxi = U(:,1:r(i+1));
+    Rxi = reshape(permute(reshape(Rxi,[r(i), m(i), r(i+1)]), [2 1 3]),m(i),[]);
+    xi = R\Rxi;
     Axi = A{i}*xi;
     Axi = reshape(Axi,n_samples,r(i),r(i+1));
-
     yl{i+1} = zeros(n_samples,r(i+1));
     for j = 1:r(i+1)
         yl{i+1}(:,j)  = sum(yl{i}.*Axi(:,:,j),2);
     end
- 
+    x{i} = reshape(permute(reshape(xi,[m(i),r(i), r(i+1)]), [2 1 3]),r(i)*m(i),[]);
+
+  
 end
 x{d} = TTcore_LS(yl{d},yr{d},A{d},b,lambda);
 end
