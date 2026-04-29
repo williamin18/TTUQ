1e7be3ee14c7927f18fa34bac63ebf338bbd0e0a
williamin18 <ziyuan.wang@mail.mcgill.ca>
2026-04-29 11:30:08 -0400

    Update Ay_denominator.m

--- TT/TTRational/TTRational_r1d/Ay_denominator.m
+++ TT/TTRational/TTRational_r1d/Ay_denominator.m
@@ -1,12 +1,24 @@
 function [Ady] = Ay_denominator(A,y,r)
 %AY_R1 undefined
 %   undefined
+    [n_samples,~] = size(A{1});
     d = length(y);
 
-    yl = cell(d,1);
-    yr = cell(d,1);
+    yl = zeros(n_samples,d);
+    yr = zeros(n_samples,d);
 
+    yl(:,1) = ones(n_samples,1);
+    for i = 2:d
+        yl(:,i) = yl(:,i-1).*(A{i-1}(:,1) + A{i-1}(:,2)*y(i-1));
+    end
+    yr(:,d) = ones(n_samples,1);
+    for i = d-1:-1:1
+        yr(:,i) = (A{i+1}(:,1) + A{i+1}(:,2)*y(i+1)).*yr(:,i+1);
+    end
+
+    Ady = zeros(n_samples,d);
     for i = 1:d
+        Ady(:,i) = yl(:,i).* A{i}(:,2).*yr(:,i);
     end
     
 end
